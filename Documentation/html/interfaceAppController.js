2625cf6b0e8f52127485c9914493243e7646d3fe
Jim Derry <balthisar@gmail.com>
2014-03-15 17:00:04 +0800

    Oops. Uncommited documentation.

--- Documentation/html/interfaceAppController.js
+++
@@ -1,4 +0,0 @@
-var interfaceAppController =
-[
-    [ "showPreferences:", "interfaceAppController.html#ae3c0df63228f12896f28f1203fecdabb", null ]
-];
--- Documentation/html/interfacePreferenceController.js
+++
@@ -1,4 +0,0 @@
-var interfacePreferenceController =
-[
-    [ "tabViewUpdates", "interfacePreferenceController.html#a73ea71937978dcd93e3ca8aae44eb7e8", null ]
-];
