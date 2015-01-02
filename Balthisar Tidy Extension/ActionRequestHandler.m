3ae5c3c7955d34b157e1cc6f610161664e4b0941
Jim Derry <balthisar@gmail.com>
2015-01-02 13:53:37 +0800

    Build target cleanup; extension work begins.

--- Balthisar Tidy Extension/ActionRequestHandler.m
+++ Balthisar Tidy Extension/ActionRequestHandler.m
@@ -1,16 +1,42 @@
-//
-//  ActionRequestHandler.m
-//  Balthisar Tidy Extension
-//
-//  Created by Jim Derry on 1/1/15.
-//  Copyright (c) 2015 Jim Derry. All rights reserved.
-//
+/**************************************************************************************************
+
+	ActionRequestHandler
+
+	Handles the Action Extension actions for Balthisar Tidy, in order to tidy selected text
+    in a host application.
+
+
+	The MIT License (MIT)
+
+	Copyright (c) 2001 to 2014 James S. Derry <http://www.balthisar.com>
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+	and associated documentation files (the "Software"), to deal in the Software without
+	restriction, including without limitation the rights to use, copy, modify, merge, publish,
+	distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
+	Software is furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in
+	all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+	BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+	NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+	DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ **************************************************************************************************/
 
 #import "ActionRequestHandler.h"
 
 @implementation ActionRequestHandler
 
-- (void)beginRequestWithExtensionContext:(NSExtensionContext *)context {
+/*–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––*
+	beginRequestWithExtensionContext
+		Tidy's the provided text and returns it.
+ *–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––*/
+- (void)beginRequestWithExtensionContext:(NSExtensionContext *)context
+{
     // Get the input item
     NSExtensionItem *item = context.inputItems.firstObject;
     NSAttributedString *content = item.attributedContentText;
