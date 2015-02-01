71ecc7757a4244ac6ff0b25430be0ea0eb0a61a0
Jim Derry <balthisar@gmail.com>
2015-02-01 14:21:39 +0800

    Deprecated tidyReleaseDate in tidylib.

--- HTMLTidy/xcode-version.h
+++ HTMLTidy/xcode-version.h
@@ -2,9 +2,5 @@
 #ifdef LIBTIDY_VERSION
 #undef LIBTIDY_VERSION
 #endif
-#ifdef RELEASE_DATE
-#undef RELEASE_DATE
-#endif
 #define LIBTIDY_VERSION "4.9.3"
-#define RELEASE_DATE "2015/02/01"
-// Updated on Sun Feb 1 12:48:10 CST 2015
+// Updated on Sun Feb 1 14:16:28 CST 2015
