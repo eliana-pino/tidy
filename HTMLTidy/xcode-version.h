429f5881049881c633ac09e4eda08f36fcbe6bd7
Jim Derry <balthisar@gmail.com>
2015-10-15 18:11:53 +0800

    Removed auto-gen'd header from tracking.

--- HTMLTidy/xcode-version.h
+++
@@ -1,8 +0,0 @@
-// This file is generated automatically. Do not edit. Edit version.txt instead.
-// The lib-tidy target Build Rules for *.txt files has produced this file.
-// Make sure you include this file as the lib-tidy target prefix header.
-#ifdef LIBTIDY_VERSION
-#undef LIBTIDY_VERSION
-#endif
-#define LIBTIDY_VERSION "5.0.0"
-// Updated on Thu Oct 15 17:50:07 CST 2015
