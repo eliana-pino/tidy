e7d889449fa0d8ae7311fcdb586b3053772c3cd7
Jim Derry <balthisar@gmail.com>
2015-10-15 16:20:16 +0800

    lib-tidy to dylib; command line build fixed for 5.0.0.

--- HTMLTidy/xcode-version.h
+++ HTMLTidy/xcode-version.h
@@ -1,6 +1,8 @@
+// This file is generated automatically. Do not edit. Edit version.txt instead.
+// The lib-tidy target Build Rules for *.txt files has produced this file.
 // Make sure you include this file as the lib-tidy target prefix header.
 #ifdef LIBTIDY_VERSION
 #undef LIBTIDY_VERSION
 #endif
 #define LIBTIDY_VERSION "5.0.0"
-// Updated on Thu Oct 15 15:40:35 CST 2015
+// Updated on Thu Oct 15 16:18:27 CST 2015
