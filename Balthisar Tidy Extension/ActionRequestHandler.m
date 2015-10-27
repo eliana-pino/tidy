3d11a7f382ad03c0187375a04d97986e395ebb5f
Jim Derry <balthisar@gmail.com>
2015-10-27 21:45:08 +0800

    Balthisar Tidy Extension documentation.

--- Balthisar Tidy Extension/ActionRequestHandler.m
+++ Balthisar Tidy Extension/ActionRequestHandler.m
@@ -41,7 +41,6 @@
 
 /*–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––*
 	beginRequestWithExtensionContext
-		Tidy's the provided text and returns it.
  *–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––*/
 - (void)beginRequestWithExtensionContext:(NSExtensionContext *)context
 {
