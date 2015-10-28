d35bdb48e92ff3b79af34b974781bd9d5cea5d5a
Jim Derry <balthisar@gmail.com>
2015-10-28 13:35:10 +0800

    Dox DocumentWindow and PreferencesWindow pass 1.

--- Balthisar Tidy/Classes/PreferencesWindow/PreferenceController.m
+++ Balthisar Tidy/Classes/PreferencesWindow/PreferenceController.m
@@ -2,35 +2,7 @@
 
 	PreferenceController
  
-	The main preference controller. Here we'll control the following:
-
-	- Handles the application preferences.
-	- Implements class methods to be used before instantiation.
- 
-	This controller is a subclass of MASPreferencesController, upon which we are piggybacking
-	to continue to use the singleton instance of this class as our main preference controller.
-	As the application preferences model is not very large or sophisticated, this is a logical
-	place to manage it for the time being.
-
-
-	The MIT License (MIT)
-
-	Copyright (c) 2003 to 2015 Jim Derry <http://www.balthisar.com>
-
-	Permission is hereby granted, free of charge, to any person obtaining a copy of this software
-	and associated documentation files (the "Software"), to deal in the Software without
-	restriction, including without limitation the rights to use, copy, modify, merge, publish,
-	distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
-	Software is furnished to do so, subject to the following conditions:
-
-	The above copyright notice and this permission notice shall be included in
-	all copies or substantial portions of the Software.
-
-	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
-	BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-	NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
-	DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+	Copyright © 2003-2015 by Jim Derry. All rights reserved.
 
  **************************************************************************************************/
 
